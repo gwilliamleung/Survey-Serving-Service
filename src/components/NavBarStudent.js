0695927b7b59041743f5db8c7fa968f13b049454
AdamJablonka <ajjablonka@gmail.com>
2023-02-01 23:38:12 -0500

    first commit

---
+++ src/components/NavBarStudent.js
@@ -0,0 +1,15 @@
+
+const NavBarStudent = () => {
+    return(
+        <div>
+            <header className="fixed w-full font-bold bg-zinc-900 z-0 p-2">
+                <div className="mx-auto flex flex-row justify-around"> 
+                    <p className="text-red-600">STUDENT VIEW</p>
+                    <button>Log out</button>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default NavBarStudent
